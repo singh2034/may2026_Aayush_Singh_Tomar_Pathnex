a844f02769198ba0a5b2e3e1cef46910970bf17b
singh2034 <singhaayush2034@gmail.com>
2026-05-06 15:20:47 +0530

    error-fix

--- coding-practice/ec2_creation.tf
+++ coding-practice/ec2_creation.tf
@@ -11,14 +11,14 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "myserver" {
+resource "aws_instance" "pathnex" {
   ami = "ami-02eb0c2388ee999f9"
   instance_type = "t3.micro"
   count = 1
   key_name = "ec2-aayush-pathnex"
-  subnet_id = ""
-  
+  subnet_id = "subnet-09800c6ca756ca941"
+  vpc_security_group_ids = ["sg-0475b55ddc724a6c8"]
   tags = {
-    Name = "myserver"
+    Name = "pathnex-ec2-1"
   }
 }
